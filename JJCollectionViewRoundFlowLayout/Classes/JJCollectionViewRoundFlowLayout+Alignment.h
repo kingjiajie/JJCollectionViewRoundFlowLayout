88d2e35e5a72c2417068955627c28a34a4b87126
kingjiajie <303559363@qq.com>
2020-01-10 15:40:10 +0800

    添加对齐方式计算分类模块

---
+++ JJCollectionViewRoundFlowLayout/Classes/JJCollectionViewRoundFlowLayout+Alignment.h
@@ -0,0 +1,16 @@
+//
+//  JJCollectionViewRoundFlowLayout+Alignment.h
+//  JJCollectionViewRoundFlowLayout
+//
+//  Created by jiajie on 2020/1/10.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JJCollectionViewRoundFlowLayout_Alignment : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ JJCollectionViewRoundFlowLayout/Classes/JJCollectionViewRoundFlowLayout+Alignment.m
@@ -0,0 +1,12 @@
+//
+//  JJCollectionViewRoundFlowLayout+Alignment.m
+//  JJCollectionViewRoundFlowLayout
+//
+//  Created by jiajie on 2020/1/10.
+//
+
+#import "JJCollectionViewRoundFlowLayout+Alignment.h"
+
+@implementation JJCollectionViewRoundFlowLayout_Alignment
+
+@end
