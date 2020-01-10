0de50e6c4335433c134530b25c392c28a092e0bc
kingjiajie <303559363@qq.com>
2020-01-10 15:41:34 +0800

    实现对齐方式计算模块

--- JJCollectionViewRoundFlowLayout/Classes/JJCollectionViewRoundFlowLayout+Alignment.h
+++ JJCollectionViewRoundFlowLayout/Classes/JJCollectionViewRoundFlowLayout+Alignment.h
@@ -6,11 +6,39 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "JJCollectionViewFlowLayoutConfig.h"
+#import "JJCollectionViewRoundFlowLayout.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface JJCollectionViewRoundFlowLayout_Alignment : NSObject
+@interface JJCollectionViewRoundFlowLayout (Alignment)
+
+
+/// 将相同y位置的cell集合到一个列表中(竖向)
+/// @param layoutAttributesAttrs layoutAttributesAttrs description
+- (NSArray *)groupLayoutAttributesForElementsByYLineWithLayoutAttributesAttrs:(NSArray *)layoutAttributesAttrs;
+
+
+/// 将相同x位置的cell集合到一个列表中(横向)
+/// @param layoutAttributesAttrs layoutAttributesAttrs description
+- (NSArray *)groupLayoutAttributesForElementsByXLineWithLayoutAttributesAttrs:(NSArray *)layoutAttributesAttrs;
+
+
+/// 根据不同对齐方式进行Cell位置计算
+/// @param layoutAttributesAttrs 传入需计算的AttributesAttrs集合列表
+/// @param toChangeAttributesAttrsList 用来保存所有计算后的AttributesAttrs
+/// @param alignmentType 对齐方式
+- (NSMutableArray *)evaluatedAllCellSettingFrameWithLayoutAttributesAttrs:(NSArray *)layoutAttributesAttrs toChangeAttributesAttrsList:(NSMutableArray *_Nonnull *_Nonnull)toChangeAttributesAttrsList cellAlignmentType:(JJCollectionViewRoundFlowLayoutAlignmentType)alignmentType;
+
+
+#pragma mark - alignment
+
+/// 计算AttributesAttrs左对齐
+/// @param layout JJCollectionViewRoundFlowLayout
+/// @param layoutAttributesAttrs 需计算的AttributesAttrs列表
+- (void)evaluatedCellSettingFrameByLeftWithWithJJCollectionLayout:(JJCollectionViewRoundFlowLayout *)layout layoutAttributesAttrs:(NSArray *)layoutAttributesAttrs;
 
 @end
+
 
 NS_ASSUME_NONNULL_END
--- JJCollectionViewRoundFlowLayout/Classes/JJCollectionViewRoundFlowLayout+Alignment.m
+++ JJCollectionViewRoundFlowLayout/Classes/JJCollectionViewRoundFlowLayout+Alignment.m
@@ -6,7 +6,95 @@
 //
 
 #import "JJCollectionViewRoundFlowLayout+Alignment.h"
+#import "JJCollectionViewFlowLayoutUtils.h"
 
-@implementation JJCollectionViewRoundFlowLayout_Alignment
+@implementation JJCollectionViewRoundFlowLayout(Alignment)
+
+/// 将相同y位置的cell集合到一个列表中(竖向)
+/// @param layoutAttributesAttrs layoutAttributesAttrs description
+- (NSArray *)groupLayoutAttributesForElementsByYLineWithLayoutAttributesAttrs:(NSArray *)layoutAttributesAttrs{
+    NSMutableDictionary *allDict = [NSMutableDictionary dictionaryWithCapacity:0];
+    for (UICollectionViewLayoutAttributes *attr  in layoutAttributesAttrs) {
+        NSMutableArray *dictArr = allDict[@(attr.frame.origin.y)];
+        if (dictArr) {
+            [dictArr addObject:[attr copy]];
+        }else{
+            NSMutableArray *arr = [NSMutableArray arrayWithObject:[attr copy]];
+            allDict[@(attr.frame.origin.y)] = arr;
+        }
+    }
+    return allDict.allValues;
+}
+
+/// 将相同x位置的cell集合到一个列表中(横向)
+/// @param layoutAttributesAttrs layoutAttributesAttrs description
+- (NSArray *)groupLayoutAttributesForElementsByXLineWithLayoutAttributesAttrs:(NSArray *)layoutAttributesAttrs{
+    NSMutableDictionary *allDict = [NSMutableDictionary dictionaryWithCapacity:0];
+    for (UICollectionViewLayoutAttributes *attr in layoutAttributesAttrs) {
+        NSMutableArray *dictArr = allDict[@(attr.frame.origin.x)];
+        if (dictArr) {
+            [dictArr addObject:[attr copy]];
+        }else{
+            NSMutableArray *arr = [NSMutableArray arrayWithObject:[attr copy]];
+            allDict[@(attr.frame.origin.x)] = arr;
+        }
+    }
+    return allDict.allValues;
+}
+
+/// 根据不同对齐方式进行Cell位置计算
+/// @param layoutAttributesAttrs 传入需计算的AttributesAttrs集合列表
+/// @param toChangeAttributesAttrsList 用来保存所有计算后的AttributesAttrs
+/// @param alignmentType 对齐方式
+- (NSMutableArray *)evaluatedAllCellSettingFrameWithLayoutAttributesAttrs:(NSArray *)layoutAttributesAttrs toChangeAttributesAttrsList:(NSMutableArray *_Nonnull *_Nonnull)toChangeAttributesAttrsList cellAlignmentType:(JJCollectionViewRoundFlowLayoutAlignmentType)alignmentType{
+    NSMutableArray *toChangeList = *toChangeAttributesAttrsList;
+    [toChangeList removeAllObjects];
+    for (NSArray *calculateAttributesAttrsArr in layoutAttributesAttrs) {
+        switch (alignmentType) {
+            case JJCollectionViewFlowLayoutAlignmentTypeByLelt:{
+                [self evaluatedCellSettingFrameByLeftWithWithJJCollectionLayout:self layoutAttributesAttrs:calculateAttributesAttrsArr];
+            }break;
+            default:
+                break;
+        }
+        [toChangeList addObjectsFromArray:calculateAttributesAttrsArr];
+    }
+    return toChangeList;
+}
+
+#pragma mark - alignment
+
+/// 计算AttributesAttrs左对齐
+/// @param layout JJCollectionViewRoundFlowLayout
+/// @param layoutAttributesAttrs 需计算的AttributesAttrs列表
+- (void)evaluatedCellSettingFrameByLeftWithWithJJCollectionLayout:(JJCollectionViewRoundFlowLayout *)layout layoutAttributesAttrs:(NSArray *)layoutAttributesAttrs{
+    //left
+    UICollectionViewLayoutAttributes *pAttr = nil;
+    for (UICollectionViewLayoutAttributes *attr in layoutAttributesAttrs) {
+        if (attr.representedElementKind != nil) {
+            //nil when representedElementCategory is UICollectionElementCategoryCell (空的时候为cell)
+            continue;
+        }
+        CGRect frame = attr.frame;
+
+        if (self.scrollDirection == UICollectionViewScrollDirectionVertical) {
+            //竖向
+            if (pAttr) {
+                frame.origin.x = pAttr.frame.origin.x + pAttr.frame.size.width + [JJCollectionViewFlowLayoutUtils evaluatedMinimumInteritemSpacingForSectionWithCollectionLayout:layout atIndex:attr.indexPath.section];
+            }else{
+                frame.origin.x = [JJCollectionViewFlowLayoutUtils evaluatedSectionInsetForItemWithCollectionLayout:layout atIndex:attr.indexPath.section].left;
+            }
+        }else{
+            //横向
+            if (pAttr) {
+                frame.origin.y = pAttr.frame.origin.y + pAttr.frame.size.height + [JJCollectionViewFlowLayoutUtils evaluatedMinimumInteritemSpacingForSectionWithCollectionLayout:layout atIndex:attr.indexPath.section];
+            }else{
+                frame.origin.y = [JJCollectionViewFlowLayoutUtils evaluatedSectionInsetForItemWithCollectionLayout:layout atIndex:attr.indexPath.section].top;
+            }
+        }
+        attr.frame = frame;
+        pAttr = attr;
+    }
+}
 
 @end
