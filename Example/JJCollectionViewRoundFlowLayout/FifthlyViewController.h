fe653441e260d5ec8b7ab4e2b4f664cde514ef29
kingjiajie <303559363@qq.com>
2020-09-24 22:58:19 +0800

    添加背景图点击事件响应相关内容

---
+++ Example/JJCollectionViewRoundFlowLayout/FifthlyViewController.h
@@ -0,0 +1,17 @@
+//
+//  FifthlyViewController.h
+//  JJCollectionViewRoundFlowLayout_Example
+//
+//  Created by jiajie on 2020/9/24.
+//  Copyright © 2020 谢家杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FifthlyViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/JJCollectionViewRoundFlowLayout/FifthlyViewController.m
@@ -0,0 +1,32 @@
+//
+//  FifthlyViewController.m
+//  JJCollectionViewRoundFlowLayout_Example
+//
+//  Created by jiajie on 2020/9/24.
+//  Copyright © 2020 谢家杰. All rights reserved.
+//
+
+#import "FifthlyViewController.h"
+
+@interface FifthlyViewController ()
+
+@end
+
+@implementation FifthlyViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
